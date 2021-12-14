cab56e31085eccb86b59ea7ec717e1aea850f9b9
Abel TRAORE <abel.traore49@gmail.com>
2021-12-14 13:12:16 +0100

    installation et configurations de démarrage

---
+++ scriptKafka/Kafka_console_producer.cmd
@@ -0,0 +1,2 @@
+:: #Démarrage console Producer :
+%KAFKA_HOME%\bin\kafka-console-producer.bat --bootstrap-server localhost:9092 --topic nom_topic
---
+++ scriptKafka/launch_Zookeeper.cmd
@@ -0,0 +1,15 @@
+:: #lancement de ZooKeeper :
+%KAFKA_HOME%\bin\windows\zookeeper-server-start.bat %KAFKA_HOME%\config\zookeeper.properties
+
+
+
+
+
+
+
+
+
+
+
+
+
