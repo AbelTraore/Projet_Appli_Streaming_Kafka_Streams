e3250b8427be844dc238d93cfdfc057c48f191b6
Abel TRAORE <abel.traore49@gmail.com>
2021-12-16 14:36:46 +0100

    développement du producer

--- src/main/scala/App_streaming.scala
+++ src/main/scala/App_streaming.scala
@@ -1,3 +1,4 @@
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.KStream
 import org.apache.kafka.streams.scala.kstream.KTable
@@ -9,8 +10,42 @@ import org.apache.kafka.streams.state._
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala.Serdes
 
+import java.util.Properties
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.common.serialization.Serdes.String
+import org.apache.kafka.streams.StreamsConfig
+
+
 
 object App_streaming {
+
+  def main(args: Array[String]): Unit = {
+
+    producer_kafka()
+
+  }
+
+
+def producer_kafka() : Unit = {
+
+  val props : Properties = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("auto.offset.reset", "latest")
+  props.put("enable.auto.commit", "false")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+
+  val producer_Kafka = new KafkaProducer[String, String](props)
+
+  val cle : String = "1"
+  val record_publish = new ProducerRecord[String, String]("streams_app", cle, "test 3")
+  producer_Kafka.send(record_publish)
+
+
+}
+
 
 
 
