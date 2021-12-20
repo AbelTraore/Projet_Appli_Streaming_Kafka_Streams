dae89b2050d35350fde5f63da9bf65e08597d4d4
Abel TRAORE <abel.traore49@gmail.com>
2021-12-20 18:51:33 +0100

    Ma première application Kafka Streams - HelloWorld

--- src/main/scala/HelloWorld_KafkaStreams.scala
+++ src/main/scala/HelloWorld_KafkaStreams.scala
@@ -1,16 +1,56 @@
 import org.apache.kafka.streams.scala._
-import org.apache.kafka.streams.scala.ImplicitConversions._
-import org.apache.kafka.streams.scala.Serdes
 
 import java.util.Properties
-import org.apache.kafka.clients.producer._
+import org.apache.kafka.streams.{StreamsConfig, Topology}
+import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.KafkaStreams
+
 import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.scala.Serdes._
 import org.apache.kafka.streams.StreamsConfig
 
 
 
 
+
+
+
 object HelloWorld_KafkaStreams {
+
+  import org.apache.kafka.streams.scala.ImplicitConversions._
+  import org.apache.kafka.streams.scala.Serdes.String
+
+  def main(args: Array[String]): Unit = {
+
+    val str : StreamsBuilder = new StreamsBuilder()
+    val kstr : KStream[String, String] = str.stream[String, String]("streams_app_upper")
+    val kstrMaj : KStream[String, String] = kstr.mapValues(v => v.toUpperCase)
+    kstrMaj.to("streams_app_upper")
+
+    val topologie : Topology = str.build()
+    val kkStream : KafkaStreams = new KafkaStreams(topologie, getParams("localhost:9092"))
+    kkStream.start()
+
+    sys.ShutdownHookThread {
+      kkStream.close()
+    }
+
+  }
+
+def getParams(bootStrapServer : String) : Properties = {
+
+  val props : Properties = new Properties()
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "hello world")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer)
+  props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, "org.apache.kafka.streams.scala.Serdes.String")
+  props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, "org.apache.kafka.streams.scala.Serdes.String")
+  props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE)
+  props.put("auto.offset.reset.config", "latest")
+
+
+  props
+
+}
 
 
 
