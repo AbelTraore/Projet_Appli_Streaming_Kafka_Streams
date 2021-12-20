a5012f10b1733601c21fcc5410354c95aa4d8cdd
Abel TRAORE <abel.traore49@gmail.com>
2021-12-20 13:57:24 +0100

    7

---
+++ src/main/scala/HelloWorld_KafkaStreams.scala
@@ -0,0 +1,17 @@
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.Serdes
+
+import java.util.Properties
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.StreamsConfig
+
+
+
+
+object HelloWorld_KafkaStreams {
+
+
+
+}
