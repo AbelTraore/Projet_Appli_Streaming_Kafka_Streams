62e33b21864a3fccf5c97573d9c1704cc6bd3590
Abel TRAORE <abel.traore49@gmail.com>
2022-06-13 18:54:02 +0200

    Processeurs Stateless de Kafka Streams

---
+++ src/main/scala/processors/stateless/WriteProcessors.scala
@@ -0,0 +1,56 @@
+package processors.stateless
+
+
+import org.apache.kafka.streams.scala._
+
+import java.util.Properties
+import org.apache.kafka.streams.{StreamsConfig, Topology}
+import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.KafkaStreams
+import schemas.{Facture, OrderLine}
+import serdes.{JSONDeserializer, JSONSerializer}
+import org.apache.kafka.common.serialization.{Serde, Serdes}
+import org.apache.kafka.streams.kstream.Printed
+
+
+object WriteProcessors extends App {
+
+  implicit val jsonSerdes: Serde[Facture] = Serdes.serdeFrom(new JSONSerializer[Facture], new JSONDeserializer)
+  implicit val consumed: Consumed[String, Facture] = Consumed.`with`(Serdes.String(), jsonSerdes)
+  implicit val produced: Produced[String, Facture] = Produced.`with`(Serdes.String(), jsonSerdes)
+
+  val props: Properties = new Properties()
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "write-processor")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+
+  val str: StreamsBuilder = new StreamsBuilder()
+  val kstrFacture: KStream[String, Facture] = str.stream[String, Facture]("factureBinJSO")
+  val kstrTotal : KStream[String, Double] = kstrFacture.mapValues(f => f.orderLine.numunits * f.orderLine.unitprice)
+
+  // selectKey()
+  val newKeys = kstrTotal.selectKey((k, t) => k.toUpperCase())
+  newKeys.print(Printed.toSysOut().withLabel("Select keys"))
+
+  // GroupByKey()
+  val kstGroupKeys = newKeys.groupByKey(Grouped.`with`(Serdes.String(), Serdes.Double()))
+
+  //GroupBy()
+  val kstrGroupBy = newKeys.groupBy((k, v) => v)(Grouped.`with`(Serdes.String(), Serdes.Double()))
+
+  // écriture dans un topic Kafka existant - opération finale
+  newKeys.to("topic_test")(Produced.`with`(Serdes.String(), Serdes.Double()))
+
+  // écriture dans un topic Kafka existant - opération non - finale
+  val t = newKeys.through("topic_test")(Produced.`with`(Serdes.String(), Serdes.Double()))
+
+  val topologie: Topology = str.build()
+  val kkStream: KafkaStreams = new KafkaStreams(topologie, props)
+  kkStream.start()
+
+  sys.ShutdownHookThread {
+    kkStream.close()
+  }
+
+
+
+}
