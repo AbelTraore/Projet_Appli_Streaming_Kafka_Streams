931d777b1502cae0c393dfd47dbaf66aed8fb94c
Abel TRAORE <abel.traore49@gmail.com>
2022-06-15 19:27:51 +0200

    Rajout et utilisation d'un state store dans une application Kafka Streams - garantir la haute disponibilité

--- src/main/scala/StateStoreDSL.scala
+++ src/main/scala/StateStoreDSL.scala
@@ -24,6 +24,16 @@ object StateStoreDSL extends App {
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, "state-store-dsl")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
 
+
+  val props_state : java.util.Map[String, String] = new java.util.HashMap[String, String]()
+  props_state.put("retention.ms","172800000" )
+  props_state.put("retention.bytes", "10000000000")
+  props_state.put("cleanup.policy", "compact")
+  props_state.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "10000")
+  props_state.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "10485760")
+
+
+
   val factureStoreName = "factureStore"
   val factureStoreSupplier : KeyValueBytesStoreSupplier = Stores.persistentKeyValueStore(factureStoreName)
   val factureStoreBuilder : StoreBuilder[KeyValueStore[String, String]] = Stores.keyValueStoreBuilder(factureStoreSupplier, String, String)
@@ -33,7 +43,11 @@ object StateStoreDSL extends App {
   str.addStateStore(factureStoreBuilder)
 
 
-  val ktblTest: KTable[String, String] = str.table("ktabletest", Materialized.as(factureStoreSupplier))
+  val ktblTest: KTable[String, String] = str.table("ktabletest", Materialized.as(factureStoreSupplier)(String, String)
+    .withCachingEnabled()
+  .withLoggingEnabled(props_state))
+
+
   ktblTest.toStream.print(Printed.toSysOut().withLabel("Clé/Valeur du KTable"))
 
 
--- src/main/scala/StateStoreProcessor.scala
+++ src/main/scala/StateStoreProcessor.scala
@@ -21,9 +21,22 @@ object StateStoreProcessor extends App {
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, "state-store-prc")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
 
+
+  val props_state : java.util.Map[String, String] = new java.util.HashMap[String, String]()
+  props_state.put("retention.ms","172800000" )
+  props_state.put("retention.bytes", "10000000000")
+  props_state.put("cleanup.policy", "compact")
+  props_state.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "10000")
+  props_state.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "10485760")
+
+
+
   val factureStoreName = "factureStore"
   val factureStoreSupplier : KeyValueBytesStoreSupplier = Stores.persistentKeyValueStore(factureStoreName)
   val factureStoreBuilder : StoreBuilder[KeyValueStore[String, String]] = Stores.keyValueStoreBuilder(factureStoreSupplier, String, String)
+    .withCachingEnabled()              // haute disponibilité
+    .withLoggingEnabled(props_state)   // tolérance aux pannes
+
   val factureStore = factureStoreBuilder.build()
 
 
