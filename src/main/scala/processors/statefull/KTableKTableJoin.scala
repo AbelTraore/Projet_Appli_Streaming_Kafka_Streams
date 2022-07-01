dab29231878ec76910e77ff42f10c26b5d5e31a3
Abel TRAORE <abel.traore49@gmail.com>
2022-07-01 23:03:42 +0200

    Jointure KTable à KTable

--- src/main/scala/processors/statefull/KTableKTableJoin.scala
+++ src/main/scala/processors/statefull/KTableKTableJoin.scala
@@ -13,7 +13,8 @@ import java.time.Duration
 import java.util.Properties
 
 
-object KstreamKstreamJoin extends App {
+// jointure KTable à KTable
+object KTableKTableJoin extends App {
 
   import org.apache.kafka.streams.scala.ImplicitConversions._
   import org.apache.kafka.streams.scala.Serdes.{String, _}
@@ -24,37 +25,34 @@ object KstreamKstreamJoin extends App {
   implicit val jsonSerdesDetailsCommandes : Serde[DetailsCommande]= Serdes.serdeFrom(new  JSONSerializer[DetailsCommande], new JSONDeserializerDtlCommandes)
   implicit val jsonSerdesCommandesComplet : Serde[CommandeComplet]= Serdes.serdeFrom(new  JSONSerializer[CommandeComplet], new JSONDeserializerCmdComplet)
 
-  implicit val consumedCommandes : Consumed[String, Commande] = Consumed.`with`(new CommandeTimeStampExtractor)(String, jsonSerdesCommandes)
-  implicit val consumedDetailsCommandes : Consumed[String, DetailsCommande] = Consumed.`with`(new DtlsTimeStampExtractor)(String, jsonSerdesDetailsCommandes)
+  implicit val consumedCommandes : Consumed[String, Commande] = Consumed.`with`(String, jsonSerdesCommandes)
+  implicit val consumedDetailsCommandes : Consumed[String, DetailsCommande] = Consumed.`with`(String, jsonSerdesDetailsCommandes)
 
   implicit val produced : Produced[String, CommandeComplet] = Produced.`with`(String,jsonSerdesCommandesComplet)
 
 
 
   val props: Properties = new Properties()
-  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kstream-kstream-join5")
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable-ktable-join")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
 
 
   val str : StreamsBuilder = new StreamsBuilder()
-  val kstrCommande : KStream[String, Commande] = str.stream[String, Commande]("commande")(consumedCommandes)
-  val kstrDtlCommande : KStream[String, DetailsCommande] = str.stream[String, DetailsCommande]("DetailsCommande")(consumedDetailsCommandes)
+  val ktCommande : KTable[String, Commande] = str.table[String, Commande]("commande")(consumedCommandes)
+  val ktDtlCommande : KTable[String, DetailsCommande] = str.table[String, DetailsCommande]("DetailsCommande")(consumedDetailsCommandes)
 
-  //kstrDtlCommande.print(Printed.toSysOut().withLabel("DetailsCommandes"))
 
-  val kstrDtlCommandeGood : KStream[String, DetailsCommande] = kstrDtlCommande.selectKey((k, v) => v.orderid) // changement de clé
-  val kjoin = kstrDtlCommandeGood.join(kstrCommande)((d : DetailsCommande, c : Commande) =>
-  {
-    CommandeComplet(d.orderid, d.productid, d.shipdate, d.billdate,
-      d.unitprice, d.numunits, d.totalprice, c.city, c.state)
-
-  }, JoinWindows.of(Duration.ofMinutes(5))
+  val  ktjoin = ktDtlCommande.join(ktCommande)(
+    (
+    (d : DetailsCommande, c : Commande) =>
+    {
+      CommandeComplet(d.orderid, d.productid, d.shipdate, d.billdate,
+        d.unitprice, d.numunits, d.totalprice, c.city, c.state)
+    })
   )
 
-  kjoin.to("commandeComplet")(produced)
 
-  kjoin.print(Printed.toSysOut().withLabel("Jointure KStreams-à-KStreams"))
-
+   ktjoin.toStream.to("commandeComplet")(produced)
 
 
   val topologie : Topology = str.build()
