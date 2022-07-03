96324f2ed5ebcfb76845ee4d1189db28cfac134f
Abel TRAORE <abel.traore49@gmail.com>
2022-07-03 18:30:15 +0200

    Réalisation des Tests Unitaires d'une Topologie Kafka Streams

--- src/main/scala/processors/stateless/FilterProcessors.scala
+++ src/main/scala/processors/stateless/FilterProcessors.scala
@@ -11,35 +11,54 @@ import schemas.{Facture, OrderLine}
 import serdes.{JSONDeserializer, JSONSerializer}
 import org.apache.kafka.common.serialization.{Serde, Serdes}
 
+import org.apache.kafka.streams.scala.Serdes.Double
+
 
 object FilterProcessors extends App {
-
-
-  implicit val jsonSerdes: Serde[Facture] = Serdes.serdeFrom(new JSONSerializer[Facture], new JSONDeserializer)
-  implicit val consumed: Consumed[String, Facture] = Consumed.`with`(Serdes.String(), jsonSerdes)
-  implicit val produced: Produced[String, Facture] = Produced.`with`(Serdes.String(), jsonSerdes)
 
   val props: Properties = new Properties()
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, "map-processor")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
 
-  val str: StreamsBuilder = new StreamsBuilder()
-  val kstrFacture: KStream[String, Facture] = str.stream[String, Facture]("factureBinJSO")
+  def topologie() : Topology = {
 
-  //utilisation de Filter() / FilterNot()
-  val kstrTotal : KStream[String, Double] = kstrFacture.mapValues(f => f.orderLine.numunits * f.orderLine.unitprice)
-  val kstrFilt = kstrTotal.filter((_,t) => t > 2000)
-
-  val kstrFiltNot = kstrTotal.filterNot((_,t) => t > 2000)
+    implicit val jsonSerdes: Serde[Facture] = Serdes.serdeFrom(new JSONSerializer[Facture], new JSONDeserializer)
+    implicit val consumed: Consumed[String, Facture] = Consumed.`with`(Serdes.String(), jsonSerdes)
+    implicit val produced: Produced[String, Double] = Produced.`with`(Serdes.String(), Double)
 
 
-  val topologie: Topology = str.build()
-  val kkStream: KafkaStreams = new KafkaStreams(topologie, props)
-  kkStream.start()
+    val str: StreamsBuilder = new StreamsBuilder()
+    val kstrFacture: KStream[String, Facture] = str.stream[String, Facture]("factureBinJSO")
 
-  sys.ShutdownHookThread {
-    kkStream.close()
+    //utilisation de Filter() / FilterNot()
+    val kstrTotal : KStream[String, Double] = kstrFacture.mapValues(f => f.orderLine.numunits * f.orderLine.unitprice)
+    //val kstrFilt = kstrTotal.filter((_,t) => t > 2000)
+
+    //val kstrFiltNot = kstrTotal.filterNot((_,t) => t > 2000)
+
+    kstrTotal.to("topic-test")(produced)
+
+
+    val topologie: Topology = str.build()
+
+    topologie
+
+
   }
+
+
+  def run() : Unit = {
+
+    val kkStream: KafkaStreams = new KafkaStreams(topologie(), props)
+    kkStream.start()
+
+    sys.ShutdownHookThread {
+      kkStream.close()
+    }
+
+  }
+
+
 
 
 }
---
+++ src/test/scala/processors/stateless/FiltersTopologyTest.scala
@@ -0,0 +1,80 @@
+package processors.stateless
+
+import org.apache.kafka.common.serialization.{DoubleDeserializer, StringDeserializer, StringSerializer}
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert._
+import org.junit.jupiter.api.{DisplayName, MethodOrderer, Order, TestMethodOrder}
+import org.apache.kafka.streams.{StreamsConfig, TopologyTestDriver}
+import org.apache.kafka.streams.test.{ConsumerRecordFactory, OutputVerifier}
+import schemas.{Facture, OrderLine}
+import serdes.{JSONDeserializer, JSONSerializer}
+
+import java.util.Properties
+
+
+@TestMethodOrder(classOf[MethodOrderer.OrderAnnotation])
+class FiltersTopolgyTest {
+
+  val props: Properties = new Properties()
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "map-processor-test")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+
+  val stringSer = new StringSerializer
+  val factureSer = new JSONSerializer[Facture]
+
+  val filterTopology = FilterProcessors
+  val testDriver = new TopologyTestDriver(filterTopology.topologie(), props)
+
+  @Before
+  def setUpAll(): Unit = {
+    testDriver
+  }
+
+
+  @Test
+  @Order(1)
+  @DisplayName("Test 1 : vérification du chiffre d'affaire de la facture")
+  def testCAFacture(): Unit = {
+    val facture1 = Facture("a326", "téléviseur LG 3A Nano", 3, 3350.75,
+      OrderLine("34e", "45i", "20/09/2010", "20/09/2010", 15.00, 700, 10))
+    val factureFactory : ConsumerRecordFactory[String, Facture] = new ConsumerRecordFactory("factureBinJSO", stringSer, factureSer)
+    testDriver.pipeInput(factureFactory.create("factureBinJSO", facture1.factureid, facture1))
+
+    // lecture du résultat
+    val resultatTopology = testDriver.readOutput("topic-test", new StringDeserializer, new DoubleDeserializer)
+
+    // test
+    assertEquals("le chiffre d'affaire de la facture doit être égal à : ", 150, resultatTopology.value().toInt)
+
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("Test 2 : vérification du chiffre d'affaire de la facture supérieure")
+  def testCAFacture2(): Unit = {
+    val facture1 = Facture("a326", "téléviseur LG 3A Nano", 3, 3350.75,
+      OrderLine("34e", "45i", "20/09/2010", "20/09/2010", 15.00, 700, 10))
+    val factureFactory : ConsumerRecordFactory[String, Facture] = new ConsumerRecordFactory("factureBinJSO", stringSer, factureSer)
+    testDriver.pipeInput(factureFactory.create("factureBinJSO", facture1.factureid, facture1))
+
+    // lecture du résultat
+    val resultatTopology = testDriver.readOutput("topic-test", new StringDeserializer, new DoubleDeserializer)
+
+    // test 2
+   // assert(150 < resultatTopology.value().toInt)
+
+  }
+
+
+
+  @After
+  def cleanUpAll(): Unit = {
+
+    testDriver.close()
+
+  }
+
+
+}
