c8bf46959ff738bdb81bacaf2b65fee1a9358087
Abel TRAORE <abel.traore49@gmail.com>
2022-07-04 10:56:17 +0200

    Deploiements d'une appli Kafka streams en prod

---
+++ src/main/scala/Deploy_prod.scala
@@ -0,0 +1,70 @@
+import org.apache.kafka.streams.scala._
+
+import java.util.Properties
+import org.apache.kafka.streams.{StreamsConfig, Topology}
+import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.KafkaStreams
+import schemas.{Facture, OrderLine}
+import serdes.{JSONDeserializer, JSONSerializer}
+import org.apache.kafka.common.serialization.{Serde, Serdes}
+import org.apache.kafka.streams.kstream.Printed
+import org.apache.kafka.streams.scala.Serdes.Double
+
+
+object Deploy_prod {
+
+  val props: Properties = new Properties()
+  props.put(StreamsConfig.APPLICATION_ID_CONFIG, "map-processor")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, "2L")
+
+
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+
+
+  def topologie() : Topology = {
+
+    implicit val jsonSerdes: Serde[Facture] = Serdes.serdeFrom(new JSONSerializer[Facture], new JSONDeserializer)
+    implicit val consumed: Consumed[String, Facture] = Consumed.`with`(Serdes.String(), jsonSerdes)
+    implicit val produced: Produced[String, Double] = Produced.`with`(Serdes.String(), Double)
+
+
+    val str: StreamsBuilder = new StreamsBuilder()
+    val kstrFacture: KStream[String, Facture] = str.stream[String, Facture]("factureBinJSO")
+
+    //utilisation de Filter() / FilterNot()
+    val kstrTotal : KStream[String, Double] = kstrFacture.mapValues(f => f.orderLine.numunits * f.orderLine.unitprice)
+    //val kstrFilt = kstrTotal.filter((_,t) => t > 2000)
+
+    //val kstrFiltNot = kstrTotal.filterNot((_,t) => t > 2000)
+
+    kstrTotal.to("topic-test")(produced)
+
+    kstrTotal.print(Printed.toSysOut().withLabel("résultat du CA"))
+
+
+    val topologie: Topology = str.build()
+
+    topologie
+
+
+  }
+
+
+  def run() : Unit = {
+
+    val kkStream: KafkaStreams = new KafkaStreams(topologie(), props)
+    kkStream.start()
+
+    sys.ShutdownHookThread {
+      kkStream.close()
+    }
+
+  }
+
+
+
+
+}
